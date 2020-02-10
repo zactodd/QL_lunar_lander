3f1838ae36ec98d8778d07c43799c4ed0d837ef8
Zac Todd <zactodd0@gmail.com>
2020-02-10 15:49:16 +1300

    tf v1 complient

--- dpn.py
+++ dpn.py
@@ -1,6 +1,8 @@
 import os
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy as np
+
+tf.disable_eager_execution()
 
 class DeepQNetwork(object):
     def __init__(self, lr, n_actions, name, input_dims,
@@ -16,31 +18,24 @@ class DeepQNetwork(object):
         self.build_network()
         self.sess.run(tf.global_variables_initializer())
         self.saver = tf.train.Saver()
-        self.checkpoint_file = os.path.join(chkpt_dir,'deepqnet.ckpt')
+        self.checkpoint_file = os.path.join(chkpt_dir, 'deepqnet.ckpt')
         self.params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
                                         scope=self.name)
+
     def build_network(self):
         with tf.variable_scope(self.name):
-            self.input = tf.placeholder(tf.float32,
-                                        shape=[None, *self.input_dims],
-                                        name='inputs')
-            self.actions = tf.placeholder(tf.float32,
-                                          shape=[None, self.n_actions],
-                                          name='action_taken')
-            self.q_target = tf.placeholder(tf.float32,
-                                           shape=[None, self.n_actions],
-                                           name='q_value')
+            self.input = tf.placeholder(tf.float32, shape=[None, *self.input_dims], name='inputs')
+            self.actions = tf.placeholder(tf.float32, shape=[None, self.n_actions], name='action_taken')
+            self.q_target = tf.placeholder(tf.float32, shape=[None, self.n_actions], name='q_value')
 
             flat = tf.layers.flatten(self.input)
             dense1 = tf.layers.dense(flat, units=self.fc1_dims,
-                                     activation=tf.nn.relu,)
+                                     activation=tf.nn.relu, )
             dense2 = tf.layers.dense(dense1, units=self.fc2_dims,
-                                     activation=tf.nn.relu,)
-            self.Q_values = tf.layers.dense(dense2, units=self.n_actions,)
-
+                                     activation=tf.nn.relu, )
+            self.Q_values = tf.layers.dense(dense2, units=self.n_actions, )
             self.loss = tf.reduce_mean(tf.square(self.Q_values - self.q_target))
             self.train_op = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
-
 
     def load_checkpoint(self):
         print("...Loading checkpoint...")
@@ -50,9 +45,10 @@ class DeepQNetwork(object):
         print("...Saving checkpoint...")
         self.saver.save(self.sess, self.checkpoint_file)
 
+
 class Agent(object):
     def __init__(self, alpha, gamma, mem_size, n_actions, epsilon, batch_size,
-                 n_games, input_dims=(210,160,4), epsilon_dec=0.996,
+                 n_games, input_dims=(210, 160, 4), epsilon_dec=0.996,
                  epsilon_end=0.01, q_eval_dir='tmp/q_eval'):
         self.action_space = [i for i in range(n_actions)]
         self.n_actions = n_actions
@@ -64,12 +60,10 @@ class Agent(object):
         self.epsilon_dec = epsilon_dec
         self.epsilon_min = epsilon_end
         self.batch_size = batch_size
-        self.q_eval = DeepQNetwork(alpha, n_actions, input_dims=input_dims,
-                                   name='q_eval', chkpt_dir=q_eval_dir)
+        self.q_eval = DeepQNetwork(alpha, n_actions, input_dims=input_dims, name='q_eval', chkpt_dir=q_eval_dir)
         self.state_memory = np.zeros((self.mem_size, *input_dims))
         self.new_state_memory = np.zeros((self.mem_size, *input_dims))
-        self.action_memory = np.zeros((self.mem_size, self.n_actions),
-                                      dtype=np.int8)
+        self.action_memory = np.zeros((self.mem_size, self.n_actions), dtype=np.int8)
         self.reward_memory = np.zeros(self.mem_size)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.int8)
 
@@ -90,8 +84,7 @@ class Agent(object):
         if rand < self.epsilon:
             action = np.random.choice(self.action_space)
         else:
-            actions = self.q_eval.sess.run(self.q_eval.Q_values,
-                      feed_dict={self.q_eval.input: state} )
+            actions = self.q_eval.sess.run(self.q_eval.Q_values, feed_dict={self.q_eval.input: state})
             action = np.argmax(actions)
 
         return action
@@ -99,7 +92,7 @@ class Agent(object):
     def learn(self):
         if self.mem_cntr > self.batch_size:
             max_mem = self.mem_cntr if self.mem_cntr < self.mem_size \
-                                    else self.mem_size
+                else self.mem_size
 
             batch = np.random.choice(max_mem, self.batch_size)
 
@@ -111,25 +104,21 @@ class Agent(object):
             new_state_batch = self.new_state_memory[batch]
             terminal_batch = self.terminal_memory[batch]
 
-            q_eval = self.q_eval.sess.run(self.q_eval.Q_values,
-                                         feed_dict={self.q_eval.input: state_batch})
-
-            q_next = self.q_eval.sess.run(self.q_eval.Q_values,
-                        feed_dict={self.q_eval.input: new_state_batch})
-
+            q_eval = self.q_eval.sess.run(self.q_eval.Q_values, feed_dict={self.q_eval.input: state_batch})
+            q_next = self.q_eval.sess.run(self.q_eval.Q_values, feed_dict={self.q_eval.input: new_state_batch})
             q_target = q_eval.copy()
             batch_index = np.arange(self.batch_size, dtype=np.int32)
 
-            q_target[batch_index,action_indices] = reward_batch + \
-                                  self.gamma*np.max(q_next, axis=1)*terminal_batch
+            q_target[batch_index, action_indices] = reward_batch + self.gamma * np.max(q_next, axis=1) * terminal_batch
 
             _ = self.q_eval.sess.run(self.q_eval.train_op,
-                            feed_dict={self.q_eval.input: state_batch,
-                                       self.q_eval.actions: action_batch,
-                                       self.q_eval.q_target: q_target})
+                                     feed_dict={
+                                        self.q_eval.input: state_batch,
+                                        self.q_eval.actions: action_batch,
+                                        self.q_eval.q_target: q_target
+                                     })
 
-            self.epsilon = self.epsilon*self.epsilon_dec if self.epsilon > \
-                           self.epsilon_min else self.epsilon_min
+            self.epsilon = self.epsilon * self.epsilon_dec if self.epsilon > self.epsilon_min else self.epsilon_min
 
     def save_models(self):
         self.q_eval.save_checkpoint()
--- lunar_lander.py
+++ lunar_lander.py
@@ -1,44 +1,43 @@
 import gym
-from dpn import DeepQNetwork, Agent
+from dpn import Agent
 from utils import plotLearning
 import numpy as np
 from gym import wrappers
 import matplotlib.pyplot as plt
-
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
     lr = 0.0005
     n_games = 500
 
-    agent = Agent(gamma=0.99, epsilon=1.0, alpha=lr, input_dims=[8],
-                  n_actions=4, mem_size=1000000, n_games=n_games,
-                  batch_size=64)
+    agent = Agent(
+        gamma=0.99,
+        epsilon=1.0,
+        alpha=lr,
+        input_dims=[8],
+        n_actions=4,
+        mem_size=1000000,
+        n_games=n_games,
+        batch_size=64
+    )
 
-    #load_checkpoint = True
-    #if load_checkpoint:
-    #    agent.load_models()
-
-    alpha = 'alpha' + str(lr)#.split('.')[1]
+    alpha = 'alpha' + str(lr)
 
     filename = '0-lunar-lander-256x256-' + alpha + '-bs64-adam-faster_decay.png'
     scores = []
     eps_history = []
 
     score = 0
-    env = wrappers.Monitor(env, "tmp/lunar-lander-4",
-                             video_callable=lambda episode_id: True, force=True)
+    env = wrappers.Monitor(env, "tmp/lunar-lander-4", video_callable=lambda episode_id: True, force=True)
 
     for i in range(n_games):
         done = False
         if i % 10 == 0 and i > 0:
-            avg_score = np.mean(scores[max(0, i-10):(i+1)])
-            print('episode: ', i,'score: ', score,
-                 ' average score %.3f' % avg_score,
-                'epsilon %.3f' % agent.epsilon)
-            #agent.save_models()
+            avg_score = np.mean(scores[max(0, i - 10):(i + 1)])
+            print('episode: ', i, 'score: ', score, ' average score %.3f' % avg_score, 'epsilon %.3f' % agent.epsilon)
+            # agent.save_models()
         else:
-            print('episode: ', i,'score: ', score)
+            print('episode: ', i, 'score: ', score)
 
         observation = env.reset()
         score = 0
@@ -54,5 +53,5 @@ if __name__ == '__main__':
         eps_history.append(agent.epsilon)
         scores.append(score)
 
-    x = [i+1 for i in range(n_games)]
+    x = [i + 1 for i in range(n_games)]
     plotLearning(x, scores, eps_history, filename)
